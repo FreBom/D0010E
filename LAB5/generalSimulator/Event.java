98ddc0f3d90b396b9b1ae391e779ccb3722a830e
aronh <aronh@DESKTOP-05HUVJH>
2017-03-04 11:50:07 +0100

    Event nästan klart

--- LAB5/generalSimulator/Event.java
+++ LAB5/generalSimulator/Event.java
@@ -8,13 +8,22 @@ public abstract class Event {
 	private double time;
 	
 	
-	public Event(Simulator state, Time time) {
+	public Event(Simulator state, double time) {
 		this.state = state;
-		this.time = time;
+		this.settime(time);
 	}
 	
 	public abstract void effect();
 	
+
+	public void settime(double time) {
+		this.time = time;
+		
+	}
+	
+	public double gettime() {
+		return time;
+	}
 	
 	
 
