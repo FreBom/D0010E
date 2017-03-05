c8625bc3562be679214a60193289afc42d58f4c7
Aron <aron.h.strandberg@gmail.com>
2017-03-05 12:11:04 +0100

    Fixade Main.

--- LAB5/main/Main.java
+++ LAB5/main/Main.java
@@ -1,8 +1,27 @@
 package main;
 
+import generalSimulator.EventStore;
+import hairdresser.HSState;
+
 public class Main {
 	
 	public static void main(String[] args){
+		
+		HSState state = new HSState();
+		EventStore store = new EventStore();
+		HaidressView view = new HaidressView(state);
+		
+		//set time
+		
+		
+		store.add(new CustomerArrives());
+		
+		// Start simulation
+		
+		Simulator simulation = new Simulator(state, store, view);
+		simulation.start();
+		
+		
 		
 	}
 
