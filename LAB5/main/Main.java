6a0b302f09ea41eb1b4ed29b844dfc1d5e825c6e
Fanny Blixt <Fanny Blixt@130.240.157.233>
2017-03-06 14:34:09 +0100

    ändrat

--- LAB5/main/Main.java
+++ LAB5/main/Main.java
@@ -2,6 +2,7 @@ package main;
 
 import generalSimulator.EventStore;
 import generalSimulator.Simulator;
+import generalSimulator.State;
 import hairdresser.Customer;
 import hairdresser.CustomerArrives;
 import hairdresser.HairdressState;
@@ -9,19 +10,22 @@ import hairdresser.FIFO;
 
 public class Main extends Simulator{
 	
+	private final int maxCustomer = 50; //maxkunder per dag
+	private final int minCustomer = 5;
 	public static void main(String[] args){		
 	   
 	}
 	
     void start() {
         EventStore store = new EventStore();
-        HairdressState state = new HairdressState();
-        HaidressView view = new HaidressView(state);
+        State state = new HairdressState();
+        HairdressView view = new HaidressView(state);
         Customer customer;
-
-        customer = hairdresser.NewCustomer.create();
-        store.add(new CustomerArrives(state, store, customer));
-    
+        int randomCustomer = (int) (Math.floor(Math.random() * (maxCustomer - minCustomer + 1)) + minCustomer);
+        for(int i = 1; i <= randomCustomer; i++){
+        	customer = hairdresser.NewCustomer.create();
+        	store.add(new CustomerArrives(state, store, customer));
+        }
         	
         }
     //k�r simulatorn som k�r alla event
