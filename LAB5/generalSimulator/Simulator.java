712838eb5553f3003b671c00dba1da7e1fbc9df1
Dexmo <Dexmo@LAPTOP-CLGJREVO>
2017-03-06 23:00:58 +0100

    Kommentar

--- LAB5/generalSimulator/Simulator.java
+++ LAB5/generalSimulator/Simulator.java
@@ -25,20 +25,26 @@ public class Simulator {
 	/**
 	 * 
 	 * @return time, <B> NOTE!: </B> it is statically typed which means we get
-	 *         the static Time, we might want to have the dynamic time
-	 *         as well...?
+	 *         the static Time, we might want to have the dynamic time as
+	 *         well...?
 	 */
 	public static double getSimTime() {// TODO we might want this method to
-										// return the dynamic time!
+										// return the dynamic time! Because now
+										// we have one time that is constant
+										// across all different types of
+										// simulations!
 		return time;
 	}
 
 	/**
 	 * Starts the simulation if there is no emergency break and there a nonempty
-	 * eventlist. 
-	 * @question1 How do we pass the first event to this method? 
-	 * @question2 How do we even know that there is a eventlist that is made? do we create all of this in the main method?
-	 * @question3 Shouldn't we just pass a new StartEvent and a StopEvent to this method?
+	 * eventlist.
+	 * 
+	 * @question1 How do we pass the first event to this method?
+	 * @question2 How do we even know that there is a eventlist that is made? do
+	 *            we create all of this in the main method?
+	 * @question3 Shouldn't we just pass a new StartEvent and a StopEvent to
+	 *            this method?
 	 */
 	public void start() {
 		view.startPrint();
