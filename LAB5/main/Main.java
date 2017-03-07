a5e36b8b43326340f08fb80851f8c237be03d2a1
Aron <aron.h.strandberg@gmail.com>
2017-03-07 19:57:29 +0100

    Vi måste tänkte om på hantering runt state och fifo

--- LAB5/main/Main.java
+++ LAB5/main/Main.java
@@ -17,13 +17,12 @@ public class Main{
 	public static void main(String[] args){		
 
         EventStore store = new EventStore();
-        FIFO fifo = new FIFO();
         State state = new HairdressState();
-        HairdressView view = new HairdressView(state, null, null, fifo);
-        
+        HairdressView view = new HairdressView(state, null, null, null); // Detta funkar ej. Måste tänka om, hantera mer via state.
+       
         Simulator sim = new Simulator(state, store, view);
 
-        store.add(new StartHSS(0.00, store, fifo));
+        store.add(new StartHSS(0.00, store, null));
         store.add(new StopHSS(999));
 
         sim.start(); 	
